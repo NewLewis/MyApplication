23e3f8924a5e5f350d37290b5272f9321c3cb3ea
newlewis <rui1218911655@gmail.com>
2019-02-22 20:29:55 +0800

    构建了welcome界面

--- app/src/main/java/com/example/rui12/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rui12/myapplication/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.rui12.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.rui12.myapplication.utils.CommonUtils;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CommonUtils commonUtils = new CommonUtils();
+        commonUtils.setStatusBar(this);
     }
 }
--- app/src/main/java/com/example/rui12/myapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/rui12/myapplication/WelcomeActivity.java
@@ -1,7 +1,12 @@
 package com.example.rui12.myapplication;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.rui12.myapplication.utils.CommonUtils;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        //设置状态栏
+        CommonUtils commonUtils = new CommonUtils();
+        commonUtils.setStatusBar(this);
+
+        //设置欢迎界面持续3s后自动转跳
+        handler.sendEmptyMessageDelayed(0,3000);
     }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+            super.handleMessage(msg);
+        }
+    };
 }
