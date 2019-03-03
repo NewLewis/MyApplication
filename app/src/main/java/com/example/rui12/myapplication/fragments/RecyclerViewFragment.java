3017849f859d1e5d05fa5b08bb65ccef7188b938
newlewis <rui1218911655@gmail.com>
2019-03-03 22:24:43 +0800

    实现了recyclerView的下拉刷新，和上拉加载

--- app/src/main/java/com/example/rui12/myapplication/fragments/RecyclerViewFragment.java
+++ app/src/main/java/com/example/rui12/myapplication/fragments/RecyclerViewFragment.java
@@ -16,6 +16,9 @@ import com.example.rui12.myapplication.R;
 import com.example.rui12.myapplication.adapter.RecyclerViewPagerAdapter;
 import com.example.rui12.myapplication.model.DreamModel;
 import com.github.florent37.materialviewpager.header.MaterialViewPagerHeaderDecorator;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,14 +30,42 @@ public class RecyclerViewFragment extends Fragment {
     final List<DreamModel> items = new ArrayList<>();
     static final int ITEMS = 9;
 
-    RecyclerView mRecyclerView;
+    private RecyclerView mRecyclerView;
+    private RefreshLayout refreshLayout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_recycleview, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_recycleview, container, false);
     }
+
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        //初始化recyclerView
+        initRecycleView(view);
+        //初始化refreshLayout
+        initRefreshLayout(view);
+    }
+
+    private void initRefreshLayout(View view){
+        refreshLayout = view.findViewById(R.id.refreshLayout);
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshlayout) {
+                Toast.makeText(getContext(),"我正在下拉刷新",Toast.LENGTH_SHORT).show();
+                refreshlayout.finishRefresh(2000/*,false*/);//传入false表示刷新失败
+            }
+        });
+        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshlayout) {
+                Toast.makeText(getContext(),"我正在上拉加载",Toast.LENGTH_SHORT).show();
+                refreshlayout.finishLoadMore(2000);//传入false表示加载失败
+            }
+        });
+    }
+
+    //初始化recyclerView
+    private void initRecycleView(View view){
         mRecyclerView=view.findViewById(R.id.recyclerView);
         for (int i=0;i<ITEMS;i++){
             items.add(new DreamModel());
